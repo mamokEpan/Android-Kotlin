814d705ae3a6115a925df992ca2e927cac2d577b
Epan <epanibs99@gmail.com>
2023-10-18 13:00:11 +0700

    update constraintLayoutView

---
+++ app/src/main/java/com/dicoding/myviewandviewgroup/MyConstraintView.kt
@@ -0,0 +1,11 @@
+package com.dicoding.myviewandviewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyConstraintView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_constraint_view)
+    }
+}
